9d008738d1335ff63b10536465b14b518720ef81
Fabien LOISON <flo@flogisoft.com>
2018-12-03 12:46:46 +0100

    adds comments and a reset key

--- 06-graphics3-background/src/main.c
+++ 06-graphics3-background/src/main.c
@@ -5,16 +5,29 @@
 void main(void) {
     UINT8 keys;
 
+    // Copy the tileset in the video memory
     set_bkg_data(0, TILESET_TILE_COUNT, TILESET);
+
+    // Copy the tilemap in the video memory
     set_bkg_tiles(0, 0, TILESET_MAP_WIDTH, TILESET_MAP_HEIGHT, TILESET_MAP);
+
+    // Display the background layer
     SHOW_BKG;
 
     while (TRUE) {
         keys = joypad();
+
+        // Scroll to the corresponding direction when a button
+        // of the D-Pad is pressed
         if (keys & J_UP) scroll_bkg(0, -1);
         if (keys & J_DOWN) scroll_bkg(0, 1);
         if (keys & J_LEFT) scroll_bkg(-1, 0);
         if (keys & J_RIGHT) scroll_bkg(1, 0);
+
+        // Reset background position when START is pressed
+        if (keys & J_START) move_bkg(0, 0);
+
+        // Wait until the screen was redrawn
         wait_vbl_done();
     }
 }
