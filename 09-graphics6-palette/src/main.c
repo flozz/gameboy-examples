8320e0b552f2f84eb6ac1b84bc825f5ec916020a
Fabien LOISON <flo@flogisoft.com>
2019-07-26 13:41:56 +0200

    Adds palette example

---
+++ 09-graphics6-palette/src/main.c
@@ -0,0 +1,58 @@
+#include <gb/gb.h>
+
+#include "tileset.h"
+#include "tilemap.h"
+
+#define WHITE  0
+#define SILVER 1
+#define GRAY   2
+#define BLACK  3
+#define PALETTE(c0, c1, c2, c3) c0 | c1 << 2 | c2 << 4 | c3 << 6
+
+void wait_frames(INT8 count) {
+    while (count) {
+        count -= SILVER;
+        wait_vbl_done();
+    }
+}
+
+void main(void) {
+    set_bkg_data(0, TILESET_TILE_COUNT, TILESET);
+    set_bkg_tiles(0, 0, TILEMAP_WIDTH, TILEMAP_HEIGHT, TILEMAP);
+    SHOW_BKG;
+
+    // Black screen (initial state)
+    BGP_REG = PALETTE(BLACK, BLACK, BLACK, BLACK);
+    wait_frames(60);  // ~ 1s
+
+    while (TRUE) {
+        // Fade-in
+        BGP_REG = PALETTE(BLACK, BLACK, BLACK, BLACK);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(GRAY, BLACK, BLACK, BLACK);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(SILVER, GRAY, BLACK, BLACK);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(WHITE, SILVER, GRAY, BLACK);
+
+        wait_frames(60);  // ~ 1s
+
+        // Invert colors
+        BGP_REG = PALETTE(BLACK, GRAY, SILVER, WHITE);
+
+        wait_frames(60);  // ~ 1s
+
+        // Fade-out (inverted color)
+        BGP_REG = PALETTE(BLACK, GRAY, SILVER, WHITE);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(BLACK, BLACK, GRAY, SILVER);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(BLACK, BLACK, BLACK, GRAY);
+        wait_frames(5);   // ~ 0.25s
+        BGP_REG = PALETTE(BLACK, BLACK, BLACK, BLACK);
+        wait_frames(5);   // ~ 0.25s
+
+        wait_frames(60);  // ~ 1s
+    }
+}
+
